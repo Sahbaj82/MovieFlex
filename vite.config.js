13320dd4785116e6bc9a5cdd4c1fef0e2faa5339
Sahbaj82 <mdsahbajalam189@gmail.com>
2026-02-09 00:30:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: process.env.VITE_BASE_PATH || "/MovieMAX/",
-  // vite.config.js
-  server: {
-    // Expose on all network interfaces
-    host: '0.0.0.0',
-    // History API Fallback
-    historyApiFallback: true,
-  }
+  base: "/MovieMAX/",
 })
-
